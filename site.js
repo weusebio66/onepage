d5d243874108c945042f21bb3f2faf4e1e594dc6
weusebio66 <47995446+weusebio66@users.noreply.github.com>
2019-03-26 22:57:47 -0400

    Add files via upload

---
+++ site.js
@@ -0,0 +1,21 @@
+console.log("External file");
+
+var city;
+var cityFrom = "";
+var cityName = " New York City";
+var x = 32;
+console.log("cityName, x: ", cityName, x);
+
+var firstname = "willie", lastname = "Eusebio";
+console.log("firstname, lastname", firstname, lastname);
+
+var s = "2019";
+var n = 2001;
+var s2 = 2019;
+console.log("age:", s - n);
+console.log("age:", s + n);
+console.log("age:", s2);
+
+var escapequotes = "This is willie's \"city\" name";
+console.log ("escapequotes: ", escapequotes);
+ 
